0c2b9844a945fcbcda22fdf4c8027dbe5fb0ae64
Brian Caffo <bcaffo@users.noreply.github.com>
2017-06-21 11:21:21 -0400

    try to get the hello script running

--- hello.sh
+++ hello.sh
@@ -1,7 +1,10 @@
 
-## get's the hello example up and running 
+## get's the hello example up and running
+## requires running the setUpPlumber script first
+
+mkdir -p /var/plumber/hello
 cp /usr/local/lib/R/site-library/plumber/examples/10-welcome/* /var/plumber/hello/
-wget https://raw.githubusercontent.com/bcaffo/alexaai/master/lectures/03_using_plumber/servicefile
+wget https://raw.githubusercontent.com/bcaffo/digitalOceanScripts/master/servicefile
 mv servicefile /etc/systemd/system/plumber-hello.service
 systemctl daemon-reload
 systemctl start plumber-hello && sleep 1
@@ -9,8 +12,11 @@ systemctl restart plumber-hello && sleep 1
 systemctl enable plumber-hello
 
 ## check the status of the plumber script
-systemctl status plumber-hello
+#systemctl status plumber-hello
 
-wget https://raw.githubusercontent.com/bcaffo/alexaai/master/lectures/03_using_plumber/conf
+wget https://raw.githubusercontent.com/bcaffo/digitalOceanScripts/master/conf
 mv conf /etc/nginx/sites-available/plumber-apis/hello.conf
 systemctl reload nginx
+
+##
+systemctl status plumber-hello
