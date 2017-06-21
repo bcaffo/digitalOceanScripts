343725a999f0401422be5291e298b9afe250a51b
Brian Caffo <bcaffo@users.noreply.github.com>
2017-06-21 14:15:37 -0400

    Update removeService.sh

--- removeService.sh
+++ removeService.sh
@@ -1,8 +1,10 @@
 #! /bin/bash
 # a shell command for removing a service
-# useage wget  
+# useage 
+# wget https://raw.githubusercontent.com/bcaffo/digitalOceanScripts/master/removeService.sh
 # chmod u+x removeService.sh
 # ./removeService SERVACENAME
+# Brian Caffo June 2017
 
 serviceName = "plumber-"$1
 
