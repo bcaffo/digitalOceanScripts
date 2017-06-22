5fabaa7dfb1f75500198470defef8593e2498003
Brian Caffo <bcaffo@gmail.com>
2017-06-22 19:36:15 +0000

    finished removeService.sh

--- removeService.sh
+++ removeService.sh
@@ -3,8 +3,16 @@
 # useage 
 # wget https://raw.githubusercontent.com/bcaffo/digitalOceanScripts/master/removeService.sh
 # chmod u+x removeService.sh
-# ./removeService SERVACENAME
+# ./removeService PATH
 # Brian Caffo June 2017
+# NOTE THAT THIS DELETES THE API FILE IN /var/plumber
 
-serviceName = "plumber-"$1
-echo $serviceName
+path=$1
+serviceName="plumber-"$path
+systemctl stop $serviceName
+systemctl disable $serviceName
+rm /etc/systemd/system/${serviceName}.service
+rm /etc/nginx/sites-available/plumber-apis/${path}.conf
+systemctl reload nginx
+rm -rf /var/plumber/$path
+
