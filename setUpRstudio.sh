99fcdafd51bda4b58ece959c4b5f8e88e58ba59e
Brian Caffo <bcaffo@users.noreply.github.com>
2017-11-19 15:37:37 -0500

    Tried to make it more general and added keras

--- setUpRstudio.sh
+++ setUpRstudio.sh
@@ -1,4 +1,5 @@
 # This is just a script version of http://deanattali.com/2015/05/09/setup-rstudio-shiny-server-digital-ocean/
+export RSTUDIO='https://s3.amazonaws.com/rstudio-dailybuilds/rstudio-server-1.1.383-amd64.deb'
 
 # get R
 sudo sh -c 'echo "deb http://cran.rstudio.com/bin/linux/ubuntu trusty/" >> /etc/apt/sources.list'
@@ -19,16 +20,16 @@ sudo apt-get -y install libcurl4-gnutls-dev
 sudo apt-get -y install libxml2-dev
 sudo apt-get -y install libssl-dev
 
-## These seem optional (shiny and devtools)
-# sudo su - -c "R -e \"install.packages('devtools', repos='http://cran.rstudio.com/')\""
-# sudo su - -c "R -e \"devtools::install_github('daattali/shinyjs')\""
+## Install shiny and devtools
+sudo su - -c "R -e \"install.packages('devtools', repos='http://cran.rstudio.com/')\""
+sudo su - -c "R -e \"install.packages('shiny', repos='http://cran.rstudio.com/')\""
 
 # more dependencies
 sudo apt-get -y install libapparmor1 gdebi-core
 
 # actually install rstudio server
-wget https://download2.rstudio.org/rstudio-server-0.99.896-amd64.deb
-sudo gdebi --non-interactive "rstudio-server-0.99.896-amd64.deb"
+wget $RSTUDIO
+sudo gdebi --non-interactive *.deb
 
 # I got this from here: https://support.rstudio.com/hc/en-us/articles/200552316-Configuring-the-Server
 # this allows the port to be 80 so that you don't have to type 8787 
@@ -36,4 +37,7 @@ sudo /bin/sh -c 'echo "www-port=80" >> /etc/rstudio/rserver.conf'
 
 sudo rstudio-server restart
 
+# install keras
+sudo su - -c "R -e \"devtools::install_github('rstudio/keras')\""
+sudo su - -c "R -e \"library(keras); install_keras()\""
 
